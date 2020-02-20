9ce81147e2f060d4f2ed13f51a14f301f7292221
December G <dgarnetsmith@gmail.com>
2020-02-20 14:24:06 -0700

    Alphabetize CSS and removed commented out lines from JavaScript as part of our refactoring

--- deck.js
+++ deck.js
@@ -23,24 +23,11 @@ class Deck {
   var cardJustClicked = event.target.id;
   var card = this.cards[cardJustClicked];
   var index = this.selectedCards.indexOf(card);
-  // if (event.target.classList.contains('selected')) {
-  //   deck.selectedCards.push(deck.cards[cardJustClicked]);
-  //
-  // } else if (event.target.classList.contains('is-flipped')) {
-  //   deck.selectedCards.splice([i], 1);
-  //   //will figure out how to select the correct index later
-  // }
 
   if (index===-1) {
     this.selectedCards.push(card);
-    console.log("i've been added to the array!");
   } else {
     this.selectedCards.splice(index, 1);
-    console.log("i've been removed");
   }
-
-  console.log(this.selectedCards);
-
-
     }
   }
