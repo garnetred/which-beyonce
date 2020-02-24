df01ffa2eea529d34ebfcc98832290ef184332d8
December G <dgarnetsmith@gmail.com>
2020-02-23 23:16:23 -0700

    Add image property to Card class

--- card.js
+++ card.js
@@ -1,10 +1,12 @@
 class Card {
-  constructor(cardID) {
+  constructor(imageName, cardID) {
     this.matchInfo = cardID;
     this.matched = false;
+    this.image = imageName;
   }
 
   match() {
     //  set matched to true
+    //if some condition, this.matched = true;
   }
 }
